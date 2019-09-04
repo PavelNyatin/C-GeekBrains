570b5524ab3d4fa6585e64ccb2609cd53c23a5b1
PavelNyatin <nyatinp@gmail.com>
2019-09-04 16:51:12 +0300

    Задание ко второму уроку на C# Нятин Павел

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать метод, возвращающий минимальное из трёх чисел
+// Нятин Павел
+using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Наименьшее число =" + Calculate(a, b, c).ToString());
+        }
+        static public int Calculate(int a, int b, int c)
+        {
+            int result = a;
+            for (int i = 0; i < 3; i++)
+            {
+                if (result < b && result < c) return result;
+                else result = b;
+                if (result < c) return result;
+                else result = c;
+            }
+            return result;
+        }
+    }
+}
---
+++ Homework2/Zadanie2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Написать метод подсчета количества цифр числа.
+//Нятин Павел
+using System;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество цифр введенного числа = " + Value(a).ToString());
+        }
+        static public int Value(int a)
+        {
+            string b = a.ToString();
+            int i = 0;
+            foreach (char x in b) i++;
+            return i;
+        }
+    }
+}
