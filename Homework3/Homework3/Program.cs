660d7dcf09d3f9a11ddecc254b4d32a4f807bf35
PavelNyatin <nyatinp@gmail.com>
2019-09-08 22:43:07 +0300

    Выполнил задания к 3му уроку. Задание №2 выполнил 2 способами

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * 3. *Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел.
+ * Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать программу, демонстрирующую все разработанные элементы класса.
+** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение
+ArgumentException("Знаменатель не может быть равен 0");
+Добавить упрощение дробей.
+Нятин Павел
+ * */
+using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую дробь");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("{0}/{1}",a,b);
+            Console.WriteLine("Введите вторую дробь");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("{0}/{1}", c, d);
+            Fraction test = new Fraction(a, b, c, d);
+            ArgumentException e = new ArgumentException("Деление на 0!");
+            if (b == 0 || d == 0) Console.WriteLine(e.Message);
+            else
+            {
+                test.Sum();
+                test.Div();
+                test.Mult();
+                test.Diff();
+                test.Simplification(a, b, "Упрощение 1й дроби");
+                test.Simplification(c,d,"Упрощение 2й дроби");
+            }
+        }
+    }
+
+
+    class Fraction
+    {
+
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+        public Fraction(int x, int y, int z, int t) { a = x; b = y; c = z; d = t; }
+        public void Sum()
+        {
+            Console.WriteLine("{0}/{1}+{2}/{3}=" + (a * d + c * b) +"/" + (b*d), a, b, c, d);
+        }
+         public void Div()
+         {
+
+            Console.WriteLine("{0}/{1}:{2}/{3}=" + (a * d) + "/" + (b * c), a, b, c, d);
+
+         }
+         public void Diff()
+         {
+            Console.WriteLine("{0}/{1}-{2}/{3}=" + (a * d - c * b) + "/" + (b * d), a, b, c, d);
+         }
+         public void Mult()
+         {
+            Console.WriteLine("{0}/{1}*{2}/{3}=" + (a * c) + "/" + (b * d), a, b, c, d);
+         }
+        public void Simplification(int a,int b,string e)
+        {
+            int x1 = a;
+            int x2 = b;
+            int i = 2;
+            while ( i<100000)
+            {
+                if (x1 % i == 0 && x2 % i == 0)
+                {
+                    x1 = x1 / i;
+                    x2 = x2 / i;
+                }
+                else i++;
+            }
+            Console.WriteLine("Упрощение {2}/{3}={0}/{1}",x1,x2,a,b);
+        }
+
+    }
+}
---
+++ Homework3/Zadanie2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+/* а) С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). Требуется подсчитать сумму всех нечетных положительных чисел.Сами числа и сумму вывести на экран, используя tryParse;
+б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные.При возникновении ошибки вывести сообщение.Напишите соответствующую функцию;
+    */
+namespace Zadanie2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> a = new List<double>();
+            Console.WriteLine("Введите число");
+            a = CheckAndConvert(Enum());
+            foreach (var i in a) Console.Write("{0}  ", i);
+            Console.WriteLine("  Сумма нечетных числел = {0}", Sum(a));
+        }
+        static public List<string> Enum()
+        {
+            List<string> a = new List<string>();
+            string k = Console.ReadLine();
+            while (k != "0")
+            {
+                a.Add(k);
+                k = Console.ReadLine();
+            }
+            return a;
+        }
+        static public double Sum(List<double> a)
+        {
+            double sum = 0;
+            foreach (double i in a)
+            {
+
+                if (i % 2 != 0 && i > 0) sum += i;
+            }
+            return sum;
+        }
+        static public List<double> CheckAndConvert(List<string> a)
+        {
+            bool check = true;
+            List<double> vihod = new List<double>();
+            foreach (string c in a)
+            {
+                check = double.TryParse(c, out double j);
+                if (check == false) Console.WriteLine("Недопустимое значение - {0}", c);
+                else vihod.Add(Convert.ToDouble(c));
+            }
+            return vihod;
+        }
+
+    }
+}
+           
+
